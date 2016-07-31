0ecc8d9df21bd3a815c19d1591e12c8bfda252f9
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2016-07-31 21:36:08 +0200

    Made a Product model

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,51 @@
+class ProductsController < ApplicationController
+  
+  def index
+    @products = Product.all
+  end
+  
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+  
+  def create
+    @product = Product.new(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @product }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update_attributes(params[:product])
+      flash[:notice] = "Successfully updated product."
+    end
+  end
+  
+   def destroy
+    Product.find(params[:id]).destroy
+    flash[:success] = "Product deleted"
+    redirect_to products_url
+  end
+end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:name, :price)
+    end
