d914f75fe412107ca0fcee850d3aad186adc3b28
Aron Strandberg <strandberg.aron@gmail.com>
2016-08-15 22:05:17 +0200

    Refactor ProductsController

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,61 +1,75 @@
 class ProductsController < ApplicationController
-  
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :create_purchase, only: [:buy, :index]
+
+  # GET /products
+  # GET /products.json
   def index
     @products = Product.all
   end
-  
-   def show
-    @product = Product.find(params[:id])
+
+  # GET /products/1
+  # GET /products/1.json
+  def show
   end
 
-
+  # GET /products/new
   def new
     @product = Product.new
   end
-  
-def create
+
+  # GET /products/1/edit
+  def edit
+  end
+
+  # POST /products
+  # POST /products.json
+  def create
     @product = Product.new(product_params)
- 
+
     respond_to do |format|
       if @product.save
-        format.html { redirect_to(@product,
-          :notice => 'Product was successfully created.') }
-        format.xml  { render :xml => @product, :status => :created,
-          :location => @product }
+        format.html { redirect_to @product, notice: 'product was successfully created.' }
+        format.json { render :show, status: :created, location: @product }
       else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @product.errors,
-          :status => :unprocessable_entity }
+        format.html { render :new }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  def edit
-    @product = Product.find(params[:id])
-  end
-
+  # PATCH/PUT /products/1
+  # PATCH/PUT /products/1.json
   def update
-    @product = Product.find(params[:id])
-    if @product.update_attributes(params[:product])
-      flash[:notice] = "Successfully updated product."
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to @product, notice: 'product was successfully updated.' }
+        format.json { render :show, status: :ok, location: @product }
+      else
+        format.html { render :edit }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
     end
   end
-  
-   def destroy
-    Product.find(params[:id]).destroy
-    flash[:success] = "Product deleted"
-    redirect_to products_url
+
+  # DELETE /products/1
+  # DELETE /products/1.json
+  def destroy
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to products_url, notice: 'product was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
-end
 
-private
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
+    end
 
+    # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
       params.require(:product).permit(:name, :price)
     end
-
-
-
-  
-
-
+end
