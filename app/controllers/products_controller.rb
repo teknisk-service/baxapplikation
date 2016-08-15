5096f65129b9a5bc84222290fc3fe305f270f865
Aron Strandberg <strandberg.aron@gmail.com>
2016-08-15 22:43:58 +0200

    fix product stuff

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-  before_action :create_purchase, only: [:buy, :index]
+  before_action :create_purchase, only: [:index]
 
   # GET /products
   # GET /products.json
@@ -66,6 +66,10 @@ class ProductsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
+    end
+
+    def create_purchase
+      @purchase = Purchase.new
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
