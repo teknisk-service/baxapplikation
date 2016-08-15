f1145474b52e2ed542ebc7b860122da13f757686
Aron Strandberg <strandberg.aron@gmail.com>
2016-08-15 22:05:38 +0200

    Refactor UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :correct_user]
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
 
-   def index
-    @users = User.all
+  def index
+    @users = User.all.sort_by { |user| user.debt }.reverse
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def new
     @user = User.new
   end
 
-    def create
+  def create
     @user = User.new(user_params)
     if @user.save
       log_in @user
@@ -26,12 +26,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
-   def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
+  def update
+    if @user.update(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
     else
@@ -39,33 +37,33 @@ class UsersController < ApplicationController
     end
   end
 
-    def destroy
-    User.find(params[:id]).destroy
+  def destroy
+    @user.destroy
     flash[:success] = "User deleted"
     redirect_to users_url
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
 
-      # Confirms a logged-in user.
+    # Confirms a logged-in user.
     def logged_in_user
       unless logged_in?
-        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
     end
 
-      def correct_user
-      @user = User.find(params[:id])
+    def correct_user
       redirect_to(root_url) unless current_user?(@user)
     end
-
-    
 end
 
