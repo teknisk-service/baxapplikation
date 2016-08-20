ac31f9c2e28e2f41054ee8d493281c936c63d8fe
Aron Strandberg <strandberg.aron@gmail.com>
2016-08-20 20:35:35 +0200

    Sort products by name in index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.all.sort_by(&:name)
   end
 
   # GET /products/1
