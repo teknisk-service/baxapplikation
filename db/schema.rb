136d518e184a8973f5edc2056232f6440e810e7d
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2017-06-27 22:41:08 +0200

    Migrated table outlays

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616202117) do
+ActiveRecord::Schema.define(version: 20170627202052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20170616202117) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "outlays", force: :cascade do |t|
+    t.integer  "sum"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "products", force: :cascade do |t|
@@ -34,6 +41,14 @@ ActiveRecord::Schema.define(version: 20170616202117) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "shoppings", force: :cascade do |t|
+    t.integer  "sum"
+    t.string   "description"
+    t.string   "mammerist"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
