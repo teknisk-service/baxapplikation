121ac69270598ee0fc88f11f502b2ccd4ae62f78
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2017-07-06 18:14:01 +0200

    More work on cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,73 +3,33 @@ class CartsController < ApplicationController
 		@carts = Cart.all 
 	end
 
-	def show
-		@cart_item = @cart.cart_items
-	end
-
 	def new
 		@cart = Cart.new
-		get_products
-	end
-
-	def edit
-		get_products
-	end
+	end	
 
 	def create
 		@cart = Cart.new(cart_params)
-		
-		params[:products][:id].each do |p|
-			if !p.empty? 
-				@cart.cart_items.build(:product_id => p)
-			end
-		end
+		if @project.save
+			flash[:notice] = 'Inköp skapat'
 
-		if @cart.save
-			redirect_to @cart
+			redirect_to carts_path
 		else
-			render 'new'
+			render :new
 		end
-		
 	end
 
 	def update
-		respond_to do |format|
-			if @cart.update(cart_params)
-				@cart.products = []
-				params[:products][:id].each do |p|
-					if !p.empty?
-						@cart.products << Product.find(p)
-					end
-				end
-				format.html { redirect_to @cart, notice: 'Author was successfully updated.' }
-				format.json { render :show, status: :ok, location: @cart }
-			else
-				format.html { render :edit }
-				format.json { render json: @cart.errors, status: :unprocessable_entity }
-			end
+		@cart = Cart.find(params[:id])  
+		if @cart.update_attributes(cart_params)
+			redirect_to @cart, notice: 'Uppdaterade inköp'
+		else
+			render :edit
 		end
 	end
 
-	def destroy
-		@cart = Cart.find(params[:id])
-		@cart.destroy
-		flash[:success] = "Inköp borttaget"
-		redirect_to carts_url
-	end
-
 	private
-	def set_cart
-		@cart = Cart.find(params[:id])
-	end
-
 	def cart_params
 		params.require(:cart).permit(:date, :mammerist)
 	end	
-
-	def get_products
-		@all_products = Product.all 
-		@cart_item = @cart.cart_items.build
-	end
 end
 
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,6 @@
 class Cart < ApplicationRecord
-	has_many :cart_items
-	has_many :products, through: :cart_items
+	has_many :cart_items, inverse_of: :cart
+	has_many :products
+
+	accepts_nested_attributes_for :cart_items, reject_if: proc { |a| a[:date].blank?}, allow_destroy: true
 end
