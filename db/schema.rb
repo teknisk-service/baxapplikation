b22200a3fc85fb5dab3921d9745c9bde2cc72756
josefinnilsson <josefin.l.nilsson@gmail.com>
2017-07-22 16:48:14 +0200

    Remove unneccesary migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170722095023) do
+ActiveRecord::Schema.define(version: 20170722094623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "cart_items", force: :cascade do |t|
-    t.integer  "cart_id"
-    t.integer  "product_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "quantity"
-  end
-
-  create_table "carts", force: :cascade do |t|
-    t.datetime "date"
-    t.string   "mammerist"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "comments", force: :cascade do |t|
     t.string   "comment"
@@ -44,11 +29,12 @@ ActiveRecord::Schema.define(version: 20170722095023) do
   end
 
   create_table "order_items", force: :cascade do |t|
+    t.integer  "order_id"
     t.integer  "quantity"
+    t.integer  "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "product_id"
-    t.integer  "order_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id", using: :btree
   end
 
   create_table "orders", force: :cascade do |t|
@@ -88,14 +74,6 @@ ActiveRecord::Schema.define(version: 20170722095023) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "shoppings", force: :cascade do |t|
-    t.integer  "sum"
-    t.string   "description"
-    t.string   "mammerist"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
@@ -104,8 +82,8 @@ ActiveRecord::Schema.define(version: 20170722095023) do
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin",           default: false
-    t.integer  "payment"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
+  add_foreign_key "order_items", "orders"
 end
