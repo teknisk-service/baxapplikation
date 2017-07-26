f97768c64a254971e3399c8271d3dd01918cef46
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2017-07-26 23:21:00 +0200

    Don't set product price on create, instead sum order item price / sum order item quantity

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,20 @@
 class Product < ApplicationRecord
   validates :name, presence: true
-  validates :price, presence: true
 
   has_many :purchases
   has_many :orders, through: :order_items
   has_many :order_items, inverse_of: :product
 
-  def total_quantity
-  	OrderItem.where(product: self).sum(:quantity)
+  def product_price 
+  	if (OrderItem.where(product: self).sum(:quantity) > 0)
+  		OrderItem.where(product: self).sum(:price) / OrderItem.where(product: self).sum(:quantity) 
+  	end
+  end
+
+  def set_price
+  	@price = self.product_price 
+  	if (@price != self.price) 
+  		update_attribute(:price, @price)
+  	end
   end
 end
