a0bf4fc7f3df8fa019efc073f86bf5f45935a8e6
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2017-07-27 09:36:06 +0200

    Always update price

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,15 +6,15 @@ class Product < ApplicationRecord
   has_many :order_items, inverse_of: :product
 
   def product_price 
-  	if (OrderItem.where(product: self).sum(:quantity) > 0)
-  		OrderItem.where(product: self).sum(:price) / OrderItem.where(product: self).sum(:quantity) 
-  	end
+    if (OrderItem.where(product: self).sum(:quantity) > 0) 
+  	 OrderItem.where(product: self).sum(:price) / OrderItem.where(product: self).sum(:quantity) 
+    else 
+      0
+    end
   end
 
   def set_price
   	@price = self.product_price 
-  	if (@price != self.price) 
-  		update_attribute(:price, @price)
-  	end
+  	update_attribute(:price, @price)
   end
 end
