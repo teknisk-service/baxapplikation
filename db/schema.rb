423483269a081a71866d734dd1b46199b12d1358
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2017-08-02 09:46:26 +0200

    Add user who payed to outlay

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170801080238) do
+ActiveRecord::Schema.define(version: 20170802074202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20170801080238) do
     t.datetime "updated_at",  null: false
     t.string   "description"
     t.integer  "sum"
+    t.integer  "user_id"
   end
 
   create_table "payments", force: :cascade do |t|
