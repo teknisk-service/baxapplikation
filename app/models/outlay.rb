067e127263dda82cfdfafeb321f3b3ede71e5484
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2017-08-02 09:41:47 +0200

    Update outlays views

--- app/models/outlay.rb
+++ app/models/outlay.rb
@@ -3,4 +3,8 @@ class Outlay < ApplicationRecord
 	has_many :users, :through => :debters, :class_name => 'User'
 	accepts_nested_attributes_for :users, :reject_if => :all_blank, :allow_destroy => true
 	accepts_nested_attributes_for :debters, :reject_if => :all_blank, :allow_destroy => true
+
+	def debters
+		Debter.all.where(outlay_id: self)
+	end
 end
