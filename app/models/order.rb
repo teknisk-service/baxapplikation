1fa844fd566a03ad9ab7090069168ebdd80d0dd4
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2017-08-08 19:51:39 +0200

    Update product price after creating an order

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,15 @@ class Order < ApplicationRecord
 	accepts_nested_attributes_for :order_items, :reject_if => :all_blank, :allow_destroy => true
 	accepts_nested_attributes_for :products, :reject_if => :all_blank, :allow_destroy => true
 
+	after_save :update_product_price
+
+	def update_product_price
+		@products = Product.all
+		@products.each do |p|
+			p.set_price
+		end
+	end
+
 	def payed_text 
 		return 'Ja' if self.payed == true
 		return 'Nej' if self.payed == false 
