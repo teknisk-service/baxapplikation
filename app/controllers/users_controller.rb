0724ac515495919291e6aeb7a027da29ff52018b
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2017-08-09 15:43:11 +0200

    Add logic for toplist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,10 @@ class UsersController < ApplicationController
   def show
   end
 
+  def baxbollstoppen
+    @toplist = User.all.sort_by(&:baxbollar).reverse
+  end
+
   def new
     @user = User.new
   end
