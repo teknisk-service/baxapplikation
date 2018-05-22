6538742f8b469d63c45e1c942aa530fa58c3715c
Aron Strandberg <aron@aronstrandberg.com>
2018-05-22 23:20:32 +0200

    Fix whitespace, I think?

--- db/schema.rb
+++ db/schema.rb
@@ -1,88 +1,114 @@
-ActiveRecord::Schema.define(version: 20180327185744) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20180327210543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string   "comment"
-    t.integer  "user_id"
+    t.string "comment"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "debters", force: :cascade do |t|
-    t.integer  "user_id"
-    t.boolean  "payed"
+    t.integer "user_id"
+    t.boolean "payed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "amount"
-    t.integer  "outlay_id"
+    t.integer "amount"
+    t.integer "outlay_id"
   end
 
   create_table "inventories", force: :cascade do |t|
-    t.integer  "total"
+    t.integer "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "order_items", force: :cascade do |t|
-    t.integer  "order_id"
-    t.integer  "quantity"
-    t.integer  "price"
+    t.integer "order_id"
+    t.integer "quantity"
+    t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "product_id"
-    t.index ["order_id"], name: "index_order_items_on_order_id", using: :btree
+    t.integer "product_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
   end
 
   create_table "orders", force: :cascade do |t|
-    t.decimal  "total"
-    t.date     "date"
-    t.string   "mammerist"
+    t.decimal "total"
+    t.date "date"
+    t.string "mammerist"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean  "payed"
+    t.boolean "payed"
   end
 
   create_table "outlays", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "description"
-    t.integer  "sum"
-    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "description"
+    t.integer "sum"
+    t.integer "user_id"
   end
 
   create_table "payments", force: :cascade do |t|
-    t.integer  "amount"
-    t.integer  "user_id"
+    t.integer "amount"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
-    t.string   "name"
-    t.decimal  "price",      precision: 10, scale: 2
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.boolean  "alcohol"
+    t.string "name"
+    t.decimal "price", precision: 10, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "alcohol"
   end
 
   create_table "purchases", force: :cascade do |t|
-    t.integer  "product_id"
-    t.integer  "user_id"
+    t.integer "product_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shoppings", id: :serial, force: :cascade do |t|
+    t.integer "sum"
+    t.string "description"
+    t.string "mammerist"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.string   "password_digest"
-    t.string   "remember_digest"
-    t.boolean  "admin",           default: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "remember_digest"
+    t.boolean "admin", default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  create_table "whitelists", id: :serial, force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "order_items", "orders"
