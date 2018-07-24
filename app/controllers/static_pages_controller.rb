e73946c230919db70a16800b85beb400c87786a3
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2018-07-24 19:50:24 +0200

    Toplist for homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
   	@purchase = Purchase.new
+    @users = User.all.sort_by(&:debt).reverse
     if current_user
   	 @user = current_user
   	 @purchases_grouped = @user.purchases_grouped
