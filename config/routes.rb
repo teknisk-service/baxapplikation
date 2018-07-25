d346ab9541faa7f381810ed24d15629c089ab5b6
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2018-07-25 19:45:23 +0200

    Land on login page

--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get  'static_pages/makeawish'
   get  '/signup',  to: 'users#new'
-  get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   get '/toplists', to: 'users#toplists'
