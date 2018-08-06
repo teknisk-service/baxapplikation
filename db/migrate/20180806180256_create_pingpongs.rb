263c9f9a463040c92b0f3d2959a035e3aa6b3f39
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2018-08-06 21:05:00 +0200

    Update schema for pingpong

--- db/migrate/20180806180256_create_pingpongs.rb
+++ db/migrate/20180806180256_create_pingpongs.rb
@@ -1,6 +1,10 @@
 class CreatePingpongs < ActiveRecord::Migration[5.1]
   def change
     create_table :pingpongs do |t|
+      t.integer :winner
+      t.integer :looser
+      t.integer :sets_p1
+      t.integer :sets_p2
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180806173959) do
+ActiveRecord::Schema.define(version: 20180806180256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,10 @@ ActiveRecord::Schema.define(version: 20180806173959) do
   end
 
   create_table "pingpongs", force: :cascade do |t|
+    t.integer "winner"
+    t.integer "looser"
+    t.integer "sets_p1"
+    t.integer "sets_p2"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
