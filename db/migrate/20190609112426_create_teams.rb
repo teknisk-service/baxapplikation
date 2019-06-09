f730eae86008ca89d99e910c39f1debadc5f6f89
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-06-09 13:27:04 +0200

    Create team model

---
+++ db/migrate/20190609112426_create_teams.rb
@@ -0,0 +1,11 @@
+class CreateTeams < ActiveRecord::Migration[5.1]
+  def change
+    create_table :teams do |t|
+      t.integer :year
+      t.boolean :active
+      t.integer :users, array: true, default: []
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180806180256) do
+ActiveRecord::Schema.define(version: 20190609112426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,14 @@ ActiveRecord::Schema.define(version: 20180806180256) do
   create_table "shared_purchases", force: :cascade do |t|
     t.string "name"
     t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.integer "year"
+    t.boolean "active"
+    t.integer "users", default: [], array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
