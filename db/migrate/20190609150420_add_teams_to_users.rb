21f7013d4375b5e62884bd20ec36f22ea90a4a59
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-07-15 21:54:41 +0200

    Change teams from int[] to string[]

--- db/migrate/20190609150420_add_teams_to_users.rb
+++ db/migrate/20190609150420_add_teams_to_users.rb
@@ -1,5 +1,5 @@
 class AddTeamsToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :teams, :integer, array: true, :default => []
+    add_column :users, :teams, :string, array: true, :default => []
   end
 end
