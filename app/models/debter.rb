1e75377d292938f1c110449349d79e9c3d9b192b
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-07-16 21:01:41 +0200

    Set team_id when creating different objects

--- app/models/debter.rb
+++ app/models/debter.rb
@@ -2,9 +2,16 @@ class Debter < ApplicationRecord
 	belongs_to :outlay
 	belongs_to :user
 	accepts_nested_attributes_for :user, :reject_if => :all_blank
+	after_create :set_team
 
-	def payed_text 
+	def payed_text
 		return 'Ja' if self.payed == true
-		return 'Nej' if self.payed == false 
+		return 'Nej' if self.payed == false
 	end
+
+	private
+  	def set_team
+    	team = SessionsController.helpers.current_team
+    	update_attribute(:team_id, team.id)
+  	end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,8 @@ class Product < ApplicationRecord
   has_many :orders, through: :order_items
   has_many :order_items, inverse_of: :product
 
+  after_create :set_team
+
   def product_price
     if total_quantity > 0
       OrderItem.where(product: self).sum(:price).to_f / total_quantity
@@ -20,5 +22,11 @@ class Product < ApplicationRecord
 
   def total_quantity
     OrderItem.where(product: self).sum(:quantity)
+  end
+
+  private
+  def set_team
+    team = SessionsController.helpers.current_team
+    update_attribute(:team_id, team.id)
   end
 end
